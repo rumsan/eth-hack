81afb2f06e3c6ef53d5e7f1ee7263c1d578daa69
UttamRajGiri <uttam.giri@rumsan.net>
2022-12-03 22:35:45 +0545

    adding error page

--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -5,6 +5,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Market from "../pages/Market";
 import Create from "../pages/Create";
+import ErrorPage from "../pages/404"
 
 import NftDetails from "../pages/NftDetails";
 
@@ -16,6 +17,7 @@ const Routers = () => {
       <Route path="/market" element={<Market />} />
       <Route path="/create" element={<Create />} />
       <Route path="/nft-detail/:id" element={<NftDetails />} />
+      <Route path="/*" element={<ErrorPage />} />
     </Routes>
   );
 };
