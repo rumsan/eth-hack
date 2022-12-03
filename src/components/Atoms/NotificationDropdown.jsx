60e4b6c229a16f89aa7cc55c3cfe40d78c93544b
UttamRajGiri <uttam.giri@rumsan.net>
2022-12-04 02:22:34 +0545

    adding notification

--- src/components/Atoms/NotificationDropdown.jsx
+++ src/components/Atoms/NotificationDropdown.jsx
@@ -4,24 +4,31 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
+  Badge,
 } from "reactstrap";
 import PropTypes from "prop-types";
 
 function Example({ direction, onClickLogout, ...args }) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [showBadge, setShowBadge] = useState(true);
 
   const toggle = () => setDropdownOpen((prevState) => !prevState);
 
+  const toggleBadge = () => {
+    setShowBadge(false);
+  };
+
   return (
     <Dropdown isOpen={dropdownOpen} toggle={toggle} direction={direction}>
-      <DropdownToggle id="wallet-dropdown">
+      <DropdownToggle id="wallet-dropdown" onClick={toggleBadge}>
         <i
           className="ri-notification-2-line text-white"
           style={{ fontSize: 20 }}
         ></i>
+        {showBadge && <Badge color="success">1</Badge>}
       </DropdownToggle>
       <DropdownMenu {...args}>
-        <DropdownItem onClick={() => onClickLogout()}>Log Out</DropdownItem>
+        <DropdownItem>notification 1</DropdownItem>
       </DropdownMenu>
     </Dropdown>
   );
